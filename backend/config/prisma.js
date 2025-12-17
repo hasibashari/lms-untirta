a96c0ae9eaacda974879542bfcb98cd6fa56a157
Hasib Ashari <hasibashari@gmail.com>
2025-12-17 22:03:39 +0700

    Add Prisma configuration for database connection

---
+++ backend/config/prisma.js
@@ -0,0 +1,11 @@
+// backend/config/prisma.js
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
