6871a347f1a948588f39342c1cecfb2a7724fbb1
Hasib Ashari <hasibashari@gmail.com>
2025-12-24 16:47:31 +0700

    feat: add CourseFooter, CourseInstructor, CourseMetaInfo, and CourseThumbnail components

---
+++ frontend/src/components/course/index.js
@@ -0,0 +1,4 @@
+export { default as CourseThumbnail } from './CourseThumbnail';
+export { default as CourseFooter } from './CourseFooter';
+export { default as CourseInstructor } from './CourseInstructor';
+export { default as CourseMetaInfo } from './CourseMetaInfo';
