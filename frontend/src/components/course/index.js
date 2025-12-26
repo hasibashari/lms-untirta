6622e66ff89eabcd375c71aa9b000c642a287bd4
Hasib Ashari <hasibashari@gmail.com>
2025-12-26 22:31:48 +0700

    feat: add StudentCourseCard export to course index for improved component accessibility

--- frontend/src/components/course/index.js
+++ frontend/src/components/course/index.js
@@ -2,3 +2,4 @@ export { default as CourseThumbnail } from './CourseThumbnail';
 export { default as CourseFooter } from './CourseFooter';
 export { default as CourseInstructor } from './CourseInstructor';
 export { default as CourseMetaInfo } from './CourseMetaInfo';
+export { default as StudentCourseCard } from './StudentCourseCard';
