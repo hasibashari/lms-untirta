2b6c8cc12c90e356e701a1f596ef6c2e1fd47029
Hasib Ashari <hasibashari@gmail.com>
2025-12-26 22:31:31 +0700

    feat: restructure MahasiswaRoute to include MyClasses and improve layout organization

--- frontend/src/routes/MahasiswaRoute.jsx
+++ frontend/src/routes/MahasiswaRoute.jsx
@@ -1,19 +1,42 @@
 import { Route } from 'react-router-dom';
 
+import MahasiswaLayout from '../layouts/MahasiswaLayout';
+import LearningLayout from '../layouts/LearningLayout';
 import MahasiswaDashboard from '../pages/mahasiswa/Dashboard';
+import MyClasses from '../pages/mahasiswa/MyClasses';
 import MahasiswaCourseHome from '../pages/mahasiswa/CourseHome';
 import CourseMaterials from '../pages/mahasiswa/CourseMaterials';
 import MaterialDetail from '../pages/mahasiswa/MaterialDetail';
 import MahasiswaAssignments from '../pages/mahasiswa/Assignments';
 import AssignmentDetail from '../pages/mahasiswa/AssignmentDetail';
 
+/**
+ * Route untuk role Mahasiswa
+ * 
+ * Struktur:
+ * - /mahasiswa/dashboard - Dashboard (overview/ringkasan)
+ * - /mahasiswa/classes - Kelas Saya (daftar lengkap)
+ * - /mahasiswa/courses/:courseId - Detail kelas
+ * - /mahasiswa/courses/:courseId/materials - Daftar materi
+ * - /mahasiswa/courses/:courseId/materials/:materialId - Detail materi (learning mode)
+ * - /mahasiswa/courses/:courseId/assignments - Daftar tugas
+ * - /mahasiswa/courses/:courseId/assignments/:assignmentId - Detail tugas
+ */
 export const MahasiswaRoute = (
   <>
-    <Route path='/mahasiswa/dashboard' element={<MahasiswaDashboard />} />
-    <Route path='/mahasiswa/courses/:courseId' element={<MahasiswaCourseHome />} />
-    <Route path='/mahasiswa/courses/:courseId/materials' element={<CourseMaterials />} />
-    <Route path='/mahasiswa/courses/:courseId/materials/:materialId' element={<MaterialDetail />} />
-    <Route path='/mahasiswa/courses/:courseId/assignments' element={<MahasiswaAssignments />} />
-    <Route path='/mahasiswa/courses/:courseId/assignments/:assignmentId' element={<AssignmentDetail />} />
+    {/* Routes dengan MahasiswaLayout (sidebar utama) */}
+    <Route element={<MahasiswaLayout />}>
+      <Route path='/mahasiswa/dashboard' element={<MahasiswaDashboard />} />
+      <Route path='/mahasiswa/classes' element={<MyClasses />} />
+      <Route path='/mahasiswa/courses/:courseId' element={<MahasiswaCourseHome />} />
+      <Route path='/mahasiswa/courses/:courseId/materials' element={<CourseMaterials />} />
+      <Route path='/mahasiswa/courses/:courseId/assignments' element={<MahasiswaAssignments />} />
+      <Route path='/mahasiswa/courses/:courseId/assignments/:assignmentId' element={<AssignmentDetail />} />
+    </Route>
+
+    {/* Routes dengan LearningLayout (fokus baca materi) */}
+    <Route element={<LearningLayout />}>
+      <Route path='/mahasiswa/courses/:courseId/materials/:materialId' element={<MaterialDetail />} />
+    </Route>
   </>
 );
