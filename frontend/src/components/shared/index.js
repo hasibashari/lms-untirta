3689634f908f31608bc538032da5f1ac31ab8e12
Hasib Ashari <hasibashari@gmail.com>
2025-12-27 15:15:49 +0700

    feat: export PageLoader component in shared index for improved accessibility

--- frontend/src/components/shared/index.js
+++ frontend/src/components/shared/index.js
@@ -1,3 +1,4 @@
 export { default as StatsGrid } from './StatsGrid';
 export { default as UserAvatars } from './UserAvatars';
 export { default as SectionHeader } from './SectionHeader';
+export { default as PageLoader } from './PageLoader';
