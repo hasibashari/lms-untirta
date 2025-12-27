6a45c5dc06b959d795a331c00fc1bf8608f927d3
Hasib Ashari <hasibashari@gmail.com>
2025-12-27 14:46:13 +0700

    feat: implement Footer component with layout and social links

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,4 +1,4 @@
-export { default as FooterBottom } from './FooterBottom';
+export { default as Footer } from './Footer'; export { default as FooterBottom } from './FooterBottom';
 export { default as FooterBrand } from './FooterBrand';
 export { default as FooterColumn } from './FooterColumn';
 export { default as FooterContact } from './FooterContact';
