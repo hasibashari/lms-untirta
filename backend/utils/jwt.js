8da90352cdceba353de09c445dae72fef7912e83
Hasib Ashari <hasibashari@gmail.com>
2026-01-31 18:04:19 +0700

    feat: remove unused utility files for JWT and password handling

--- backend/utils/jwt.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const generateToken = payload => {
-  // Payload biasanya berisi ID dan Role
-  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' }); // Token berlaku 1 jam
-};
-
-export { generateToken };
